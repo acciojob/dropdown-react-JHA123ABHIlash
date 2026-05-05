a2658c756bc935aa040ff388796fd6206f4ac54d
Abhilash Jha <abhilashjha121@gmail.com>
2026-05-05 09:23:39 +0530

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const states = [{
 	name : "Madhya Pradesh",
-	description:"",
+	description:", a large state in central India, retains landmarks from eras throughout Indian history.",
 	city :[{
 		name : "Indore",
 		description:"Indore is a city in west-central India. It’s known for the 7-story Rajwada Palace and the Lal Baag Palace, which date back to Indore’s 19th-century Holkar dynasty.",
